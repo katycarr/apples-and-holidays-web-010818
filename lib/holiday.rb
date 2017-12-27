87dac9803107fb8b2b394250a7f8c54f2148b21b
Katy Carr <khcarr9@gmail.com>
2017-12-27 16:50:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
     season_name = season
     value.each do |holiday, value|
       holiday_name = holiday
-      supplies = value.split(", ")
+      supplies = value.join(", ")
     end
     puts "#{season}:
       #{holiday}: #{supplies}"
